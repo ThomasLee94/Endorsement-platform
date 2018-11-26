118e6daa5c78e24b624b12df6560e73465497e15
Thomas Lee <thomaslee9474@gmail.com>
2018-11-25 21:24:10 -0800

    fixed require issue for models

--- bin/populate-sklls.js
+++ bin/populate-sklls.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const Skill = require('../models/skills')
+const Skill = require('../models/skill')
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/endorsement-platform', { useNewUrlParser: true });
 
