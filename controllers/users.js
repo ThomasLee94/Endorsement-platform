b24bded848657f301978b5d3b1b0568d69b08e2c
Stephen Ouyang <xisouyang@gmail.com>
2018-11-25 21:14:35 -0800

    added some styling

--- controllers/users.js
+++ controllers/users.js
@@ -7,7 +7,6 @@ module.exports = (app) => {
     app.post('/skills/users', (req, res) => {
         User.create(req.body).then(user => {
             console.log(req.body)
-            console.log("working")
             res.redirect(`/skills/${user.skillId}`);
         }).catch((err) => {
             console.log(err.message)
