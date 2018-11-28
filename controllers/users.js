71f6ca745439f04d670634a25cb2d66ba2e91994
Stephen Ouyang <xisouyang@gmail.com>
2018-11-28 14:17:48 -0800

    user form now seperated from user display page, added upvote functionality

--- controllers/users.js
+++ controllers/users.js
@@ -10,7 +10,26 @@ module.exports = (app) => {
         })
     })
 
+    app.put('/skills/:skillId/users/:userId/upvotes', (req, res) => {
+
+        //create instruction on what to update
+        const whatToUpdate = {$inc : {'upVotes' : 1}};
+
+        //pass instruction to mongo so it knows what to update
+        User.findOneAndUpdate({_id: req.params.userId}, whatToUpdate).then(user => {
+
+            //update the object to reflect the update in the model
+            user.upVotes += 1
+
+            //converts object to JSON object and sends it back to the webpage that made the request
+            res.json(user);
+            console.log(user)
+        }).catch(err => {
+            console.error(err.message)
+        })
+    })
     app.post('/skills/users', (req, res) => {
+
         User.create(req.body).then(user => {
             console.log(req.body)
             res.redirect(`/skills/${user.skillId}`);
