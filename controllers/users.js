378046e88a167a6c42ad57ab613db22fa552207b
Stephen Ouyang <xisouyang@gmail.com>
2018-11-28 12:31:12 -0800

    moved user form

--- controllers/users.js
+++ controllers/users.js
@@ -4,6 +4,12 @@ let User = require("../models/user")
 
 module.exports = (app) => {
 
+    app.get('/skills/:skillId/users/new', (req, res) => {
+        Skill.findById(req.params.skillId).then((skill) => {
+            res.render('user-form', {skill: skill})
+        })
+    })
+
     app.post('/skills/users', (req, res) => {
         User.create(req.body).then(user => {
             console.log(req.body)
